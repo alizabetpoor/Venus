42a25cd0b2548f7e699ad77c80f21b2c7263e933
alizabetpoor <ali_0_zabetpoor@ymail.com>
2022-05-22 23:00:06 +0430

    edit folder structure & App component

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import {SafeAreaView, View} from 'react-native';
+import React from 'react';
+const App: React.FC = () => {
+  <SafeAreaView>
+    <View></View>
+  </SafeAreaView>;
+};
+
+export default App;
