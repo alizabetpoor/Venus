f81bc27b9ded9a6781c6d385068992a6bc5488e2
unknown <zia.s69@gmail.com>
2022-05-25 19:54:33 +0430

    return for app.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import {SafeAreaView, View} from 'react-native';
 import React from 'react';
+
 const App: React.FC = () => {
-  <SafeAreaView>
-    <View></View>
-  </SafeAreaView>;
+    return (
+        <SafeAreaView>
+            <View></View>
+        </SafeAreaView>
+    );
 };
 
 export default App;
