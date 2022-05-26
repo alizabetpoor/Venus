22c76a7e63236439a3113a6cc612338b97f0b530
Amir Sadri <amirsadriofficial@gmail.com>
2022-05-26 18:25:59 +0430

    handle header(input and theme button) component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import {SafeAreaView, View} from 'react-native';
-import Search from './components/search/Search';
-import Card from './components/card/Card';
 import React from 'react';
 
 const App: React.FC = () => {
     return (
         <SafeAreaView>
-            <View>
-                <Search />
-                <Card />
-            </View>
+            <View></View>
         </SafeAreaView>
     );
 };
