c8b9009ca1ebf98177d18365012ce0451ec7636f
Amir Sadri <amirsadriofficial@gmail.com>
2022-05-26 18:21:53 +0430

    handle header(input and theme button) component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import {SafeAreaView, View} from 'react-native';
+import Search from './components/search/Search';
+import Card from './components/card/Card';
 import React from 'react';
 
 const App: React.FC = () => {
     return (
         <SafeAreaView>
-            <View></View>
+            <View>
+                <Search />
+                <Card />
+            </View>
         </SafeAreaView>
     );
 };
---
+++ src/components/search/Search.style.js
@@ -0,0 +1,40 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  headerBox: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  searchBox: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginLeft: 16,
+    marginVertical: 24,
+    height: 60,
+    borderRadius: 10,
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+    backgroundColor: '#fff',
+  },
+  searchText: {
+    minWidth: 230,
+    borderRightColor: '#aaa',
+    borderRightWidth: 1,
+    marginRight: 7,
+  },
+  themeButton: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 8,
+    marginVertical: 24,
+    height: 60,
+    borderRadius: 10,
+    paddingHorizontal: 16,
+    backgroundColor: '#fff',
+  }
+});
