dff908ffacdc8749aecbc23fb222d45dfe05f337
alizabetpoor <ali_0_zabetpoor@ymail.com>
2022-05-27 19:02:18 +0430

    use themeprovider & animation provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import {SafeAreaView, View} from 'react-native';
+import {ThemeProvider} from './themes/ThemeProvider';
 import React from 'react';
+import Card from './components/card/Card';
+import {AnimationProvider} from './components/card/AnimationProvider';
 const App: React.FC = () => {
-  <SafeAreaView>
-    <View></View>
-  </SafeAreaView>;
+  return (
+    <SafeAreaView>
+      <ThemeProvider>
+        <AnimationProvider>
+          <Card />
+        </AnimationProvider>
+      </ThemeProvider>
+    </SafeAreaView>
+  );
 };
 
 export default App;
