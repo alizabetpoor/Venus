ba9f708aafecdd1486e037ddfecf422ff05a9d7c
alizabetpoor <ali_0_zabetpoor@ymail.com>
2022-05-27 19:06:18 +0430

    create style of card detail

---
+++ src/components/card/CardDetail.style.js
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  detail: {
+    marginTop: 4,
+    marginHorizontal: 4,
+    borderTopColor: '#E6BFF3',
+    paddingTop: 8,
+    borderTopWidth: 1,
+    paddingBottom: 15,
+  },
+  detailElement: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+    justifyContent: 'flex-start',
+    paddingVertical: 14,
+  },
+  detailElementText: {
+    paddingLeft: 9,
+  },
+  homeText: {
+    width: '80%',
+  },
+});
