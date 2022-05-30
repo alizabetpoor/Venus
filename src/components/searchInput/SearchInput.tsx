7362ac4db0860eed6e909239261ea97f71106faa
Marzieh Valizadeh <sureh1937@gmail.com>
2022-05-30 20:33:00 +0430

    feat: add toggle theme to button

--- src/components/searchInput/SearchInput.tsx
+++ src/components/searchInput/SearchInput.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import {useContext} from 'react';
+import {Pressable} from 'react-native';
 import {View, TextInput, TouchableOpacity} from 'react-native';
 import FeatherIcon from 'react-native-vector-icons/Feather';
+import {ThemeContext} from '../../themes/ThemeProvider.js';
 import styles from './SearchInput.style.js';
 
 const SearchInput = () => {
-  const searchIcon = <FeatherIcon name="search" size={20} color="#777" />;
-  const sunIcon = <FeatherIcon name="sun" size={20} color="#777" />;
+  const {themeToggle, dark} = useContext(ThemeContext);
+  const searchIcon = <FeatherIcon name="search" size={20} color="#aaa" />;
+  const sunIcon = <FeatherIcon name="sun" size={20} color="#aaa" />;
+  const moonIcon = <FeatherIcon name="moon" size={20} color="#aaa" />;
 
   return (
     <View style={styles.headerBox}>
@@ -14,13 +19,13 @@ const SearchInput = () => {
           style={styles.searchText}
           placeholder="search users ..."
           maxLength={30}
-          // onChangeText={onChangeNumber}
-          // keyboardType="numeric"
         />
         <TouchableOpacity activeOpacity={0.6}>{searchIcon}</TouchableOpacity>
       </View>
       <TouchableOpacity activeOpacity={0.6} style={styles.themeButton}>
-        {sunIcon}
+        <Pressable style={styles.pressable} onPress={() => themeToggle()}>
+          {dark ? sunIcon : moonIcon}
+        </Pressable>
       </TouchableOpacity>
     </View>
   );
