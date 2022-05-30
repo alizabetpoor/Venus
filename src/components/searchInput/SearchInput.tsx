10329eb16eff2642d9b62da5df26303bc5aed6b2
Marzieh Valizadeh <sureh1937@gmail.com>
2022-05-30 20:57:55 +0430

    refactor searchInput component and add styles based on theme

--- src/components/searchInput/SearchInput.tsx
+++ src/components/searchInput/SearchInput.tsx
@@ -6,24 +6,42 @@ import FeatherIcon from 'react-native-vector-icons/Feather';
 import {ThemeContext} from '../../themes/ThemeProvider.js';
 import styles from './SearchInput.style.js';
 
-const SearchInput = () => {
-  const {themeToggle, dark} = useContext(ThemeContext);
-  const searchIcon = <FeatherIcon name="search" size={20} color="#aaa" />;
-  const sunIcon = <FeatherIcon name="sun" size={20} color="#aaa" />;
-  const moonIcon = <FeatherIcon name="moon" size={20} color="#aaa" />;
+const SearchInput: React.FC = () => {
+  const {themeToggle, dark, theme} = useContext(ThemeContext);
+
+  const fatherIconConstructor = (
+    name: string,
+    size: number,
+    color: string,
+  ): React.ReactNode => {
+    return <FeatherIcon name={name} size={size} color={color} />;
+  };
+  const searchIcon = fatherIconConstructor('search', 20, theme.colors.gray);
+  const sunIcon = fatherIconConstructor('sun', 20, theme.colors.gray);
+  const moonIcon = fatherIconConstructor('moon', 20, theme.colors.gray);
 
   return (
     <View style={styles.headerBox}>
-      <View style={styles.searchBox}>
+      <View
+        style={[
+          styles.searchBox,
+          {backgroundColor: theme.colors.background_1},
+        ]}>
         <TextInput
           style={styles.searchText}
+          placeholderTextColor={theme.colors.gray}
           placeholder="search users ..."
           maxLength={30}
         />
         <TouchableOpacity activeOpacity={0.6}>{searchIcon}</TouchableOpacity>
       </View>
-      <TouchableOpacity activeOpacity={0.6} style={styles.themeButton}>
-        <Pressable style={styles.pressable} onPress={() => themeToggle()}>
+      <TouchableOpacity
+        activeOpacity={0.6}
+        style={[
+          styles.themeButton,
+          {backgroundColor: theme.colors.background_1},
+        ]}>
+        <Pressable onPress={() => themeToggle()}>
           {dark ? sunIcon : moonIcon}
         </Pressable>
       </TouchableOpacity>
