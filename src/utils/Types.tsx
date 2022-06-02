f5c15e30c3110dbc7589ea8ff54f076db4fbc0eb
alizabetpoor <ali_0_zabetpoor@ymail.com>
2022-06-02 15:44:38 +0430

    add types

--- src/utils/Types.tsx
+++ src/utils/Types.tsx
@@ -1,13 +1,31 @@
 export type cardInfoProps = {
-    userDetail: {
-        [key: string]: any;
-    };
+  userDetail: {
+    [key: string]: any;
+  };
 };
 
 export type cardDetailInfoProps = {
-    userDetail: {
-        [key: string]: any;
-    };
-    animatedHeight: any;
+  userDetail: {
+    [key: string]: any;
+  };
+  animatedHeight: any;
 };
 
+export type homeProps = {
+  getUsers: (usersNumber: number) => void;
+  loading: boolean;
+  users: [];
+  refetch: () => void;
+  searchResults: any[];
+};
+export type LabelProps = {
+  text?: string;
+  textStyle?: any;
+  icon?: any;
+  iconStyle?: any;
+  textWrapperStyle?: any;
+  iconWrapperStyle?: any;
+  labelStyle?: any;
+  isIcon?: boolean;
+  isText?: boolean;
+};
