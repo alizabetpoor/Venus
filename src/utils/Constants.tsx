c7f1f75e13ba7259b962d05811e9028ed50f5e45
alizabetpoor <ali_0_zabetpoor@ymail.com>
2022-06-02 21:20:34 +0430

    add featherIconConstructor

--- src/utils/Constants.tsx
+++ src/utils/Constants.tsx
@@ -1,11 +1,19 @@
 import EvilIcon from 'react-native-vector-icons/EvilIcons';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
 import React from 'react';
 export const evilIconConstructor = (
   name: string,
   size: number,
   color: string,
 ): React.ReactNode => <EvilIcon name={name} size={size} color={color} />;
-
+export const featherIconConstructor = (
+  name: string,
+  size: number,
+  color: string,
+): React.ReactNode => {
+  return <FeatherIcon name={name} size={size} color={color} />;
+};
 export const fullNameConstructor = (
   firstName: string,
   lastName: string,
