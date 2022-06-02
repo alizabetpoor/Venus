552a3bd73e1746d2d37bb3d7f88eaf854642f528
Ziya <s.zia@isaco.ir>
2022-06-02 11:14:28 +0430

    refactor and change some name of file

--- src/components/card/detail/index.tsx
+++ src/components/card/detail/index.tsx
@@ -4,69 +4,58 @@ import FeatherIcon from 'react-native-vector-icons/Feather';
 import OctIcons from 'react-native-vector-icons/Octicons';
 import styles from './CardDetail.style.js';
 import {ThemeContext} from '../../../themes/ThemeProvider';
+import {cardDetailInfoProps} from '../../../utils/Types';
 
-type detailInfoProps = {
-  userDetail: {
-    [key: string]: any;
-  };
-  animatedHeight: any;
-};
-const CardDetail: React.FC<detailInfoProps> = ({
-  userDetail,
-  animatedHeight,
-}) => {
-  const interpolatedHeight = animatedHeight.interpolate({
-    inputRange: [0, 100],
-    outputRange: ['0%', '100%'],
-  });
-  const {theme} = useContext(ThemeContext);
-  const phoneIcon = (
-    <FeatherIcon name="phone" size={20} color={theme.colors.text_icon} />
-  );
-  const mailIcon = (
-    <OctIcons name="mail" size={20} color={theme.colors.text_icon} />
-  );
-  const homeIcon = (
-    <OctIcons name="home" size={20} color={theme.colors.text_icon} />
-  );
-  const address: string = `${userDetail.location.street.name},\
-   ${userDetail.location.street.number}\
-   ${userDetail.location.city},${userDetail.location.state}\
-    postcode: ${userDetail.location.postcode}`;
-  return (
-    <Animated.View
-      style={[
-        styles.detail,
-        {
-          maxHeight: interpolatedHeight,
-          borderTopColor: theme.colors.text_icon,
-        },
-      ]}>
-      <View style={[styles.detailElement]}>
-        {phoneIcon}
-        <Text style={[styles.detailElementText, {color: theme.colors.text}]}>
-          {userDetail.phone}
-        </Text>
-      </View>
-      <View style={[styles.detailElement]}>
-        {mailIcon}
-        <Text style={[styles.detailElementText, {color: theme.colors.text}]}>
-          {userDetail.email}
-        </Text>
-      </View>
-      <View style={[styles.detailElement]}>
-        {homeIcon}
-        <Text
-          style={[
-            styles.detailElementText,
-            styles.homeText,
-            {color: theme.colors.text},
-          ]}>
-          {address}
-        </Text>
-      </View>
-    </Animated.View>
-  );
+const CardDetail: React.FC<cardDetailInfoProps> = ({userDetail, animatedHeight}) => {
+    const interpolatedHeight = animatedHeight.interpolate({
+        inputRange: [0, 100],
+        outputRange: ['0%', '100%'],
+    });
+    const {theme} = useContext(ThemeContext);
+    // @ts-ignore
+    const phoneIcon = <FeatherIcon name="phone" size={20} color={theme.colors.text_icon}/>
+    // @ts-ignore
+    const mailIcon = <OctIcons name="mail" size={20} color={theme.colors.text_icon}/>
+    // @ts-ignore
+    const homeIcon = <OctIcons name="home" size={20} color={theme.colors.text_icon}/>
+    const address: string = `${userDetail.location.street.name},\
+                               ${userDetail.location.street.number}\
+                               ${userDetail.location.city},${userDetail.location.state}\
+                                postcode: ${userDetail.location.postcode}`;
+    return (
+        <Animated.View
+            style={[
+                styles.detail,
+                {
+                    maxHeight: interpolatedHeight,
+                    borderTopColor: theme.colors.text_icon,
+                },
+            ]}>
+            <View style={[styles.detailElement]}>
+                {phoneIcon}
+                <Text style={[styles.detailElementText, {color: theme.colors.text}]}>
+                    {userDetail.phone}
+                </Text>
+            </View>
+            <View style={[styles.detailElement]}>
+                {mailIcon}
+                <Text style={[styles.detailElementText, {color: theme.colors.text}]}>
+                    {userDetail.email}
+                </Text>
+            </View>
+            <View style={[styles.detailElement]}>
+                {homeIcon}
+                <Text
+                    style={[
+                        styles.detailElementText,
+                        styles.homeText,
+                        {color: theme.colors.text},
+                    ]}>
+                    {address}
+                </Text>
+            </View>
+        </Animated.View>
+    );
 };
 
 export default CardDetail;
--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,109 +1,92 @@
-import React, {useContext, useRef} from 'react';
-
+import React, {useContext, useRef} from 'react'
 import {View, Text, Image, TouchableOpacity, Animated} from 'react-native';
 import styles from './Card.style.js';
 import EvilIcon from 'react-native-vector-icons/EvilIcons';
 import {ThemeContext} from '../../themes/ThemeProvider.js';
 import CardDetail from './detail';
 import {AnimationContext} from './AnimationProvider.js';
+import {cardInfoProps} from '../../utils/Types';
 
-type cardInfoProps = {
-  userDetail: {
-    [key: string]: any;
-  };
-};
+const Card: React.FC<cardInfoProps> = ({userDetail, index, isActive, toggleOff}) => {
+    const {OpenCardAnimation, CloseCardAnimation, animationDuration} =
+        useContext(AnimationContext);
+    const animatedHeight = useRef(new Animated.Value(0)).current;
+    const animatedIcon = useRef(new Animated.Value(0)).current;
+    const {theme} = useContext(ThemeContext);
 
-const Card: React.FC<cardInfoProps> = ({
-  userDetail,
-  index,
-  isActive,
-  toggleOff,
-}) => {
-  const {OpenCardAnimation, CloseCardAnimation, animationDuration} =
-    useContext(AnimationContext);
-  const animatedHeight = useRef(new Animated.Value(0)).current;
-  const animatedIcon = useRef(new Animated.Value(0)).current;
-  const {theme} = useContext(ThemeContext);
+    const toggleCard = (): void => {
+        if (isActive == true) {
+            //close dropdown
+            CloseCardAnimation(animatedIcon, animatedHeight);
+            setTimeout(() => {
+                toggleOff('');
+            }, animationDuration - 100);
+        } else {
+            // open dropdown
+            OpenCardAnimation(animatedIcon, animatedHeight);
+            toggleOff(userDetail.login.uuid);
+        }
+    };
 
-  const toggleCard = (): void => {
-    if (isActive == true) {
-      //close dropdown
-      CloseCardAnimation(animatedIcon, animatedHeight);
-      setTimeout(() => {
-        toggleOff('');
-      }, animationDuration - 100);
-    } else {
-      // open dropdown
-      OpenCardAnimation(animatedIcon, animatedHeight);
-      toggleOff(userDetail.login.uuid);
-    }
-  };
+    const evilIconConstructor = (
+        name: string,
+        size: number,
+        color: string,
+    ): React.ReactNode => <EvilIcon name={name} size={size} color={color}/>;
 
-  const interpolatedIcon = animatedIcon.interpolate({
-    inputRange: [0, 100],
-    outputRange: ['0deg', '90deg'],
-  });
-
-  const evilIconConstructor = (
-    name: string,
-    size: number,
-    color: string,
-  ): React.ReactNode => {
-    return <EvilIcon name={name} size={size} color={color} />;
-  };
-  const locationIcon = evilIconConstructor(
-    'location',
-    20,
-    theme.colors.text_icon,
-  );
-  const bottomIcon = evilIconConstructor(
-    'chevron-down',
-    30,
-    theme.colors.text_icon,
-  );
-  const rightIcon = evilIconConstructor(
-    'chevron-right',
-    30,
-    theme.colors.text_icon,
-  );
-  return (
-    <View
-      key={index}
-      style={[styles.card, {backgroundColor: theme.colors.background_1}]}>
-      <TouchableOpacity
-        activeOpacity={0.6}
-        style={[styles.info]}
-        onPress={() => toggleCard()}>
-        <View style={[styles.leftContainer]}>
-          <Image
-            style={[styles.photo]}
-            resizeMode="contain"
-            source={{
-              uri: userDetail.picture.large,
-            }}
-          />
-          <View style={[styles.userinfo]}>
-            <Text style={[styles.name, {color: theme.colors.text}]}>
-              {userDetail.name.first + ' ' + userDetail.name.last}{' '}
-            </Text>
-            <Text style={[styles.username, {color: theme.colors.text}]}>
-              @{userDetail.login.username}
-            </Text>
-          </View>
+    const locationIcon = evilIconConstructor(
+        'location',
+        20,
+        theme.colors.text_icon,
+    );
+    const bottomIcon = evilIconConstructor(
+        'chevron-down',
+        30,
+        theme.colors.text_icon,
+    );
+    const rightIcon = evilIconConstructor(
+        'chevron-right',
+        30,
+        theme.colors.text_icon,
+    );
+    return (
+        <View
+            key={index}
+            style={[styles.card, {backgroundColor: theme.colors.background_1}]}>
+            <TouchableOpacity
+                activeOpacity={0.6}
+                style={[styles.info]}
+                onPress={() => toggleCard()}>
+                <View style={[styles.leftContainer]}>
+                    <Image
+                        style={[styles.photo]}
+                        resizeMode="contain"
+                        source={{
+                            uri: userDetail.picture.large,
+                        }}
+                    />
+                    <View style={[styles.userinfo]}>
+                        <Text style={[styles.name, {color: theme.colors.text}]}>
+                            {userDetail.name.first + ' ' + userDetail.name.last}{' '}
+                        </Text>
+                        <Text style={[styles.username, {color: theme.colors.text}]}>
+                            @{userDetail.login.username}
+                        </Text>
+                    </View>
+                </View>
+                <View style={[styles.rightContainer]}>
+                    {locationIcon}
+                    <Text style={[styles.country, {color: theme.colors.text_icon}]}>
+                        {userDetail.location.country}
+                    </Text>
+                    {!isActive ? rightIcon : bottomIcon}
+                </View>
+            </TouchableOpacity>
+            {isActive && (
+                <CardDetail userDetail={userDetail} animatedHeight={animatedHeight}/>
+            )}
         </View>
-        <View style={[styles.rightContainer]}>
-          {locationIcon}
-          <Text style={[styles.country, {color: theme.colors.text_icon}]}>
-            {userDetail.location.country}
-          </Text>
-          {!isActive ? rightIcon : bottomIcon}
-        </View>
-      </TouchableOpacity>
-      {isActive && (
-        <CardDetail userDetail={userDetail} animatedHeight={animatedHeight} />
-      )}
-    </View>
-  );
+    );
 };
 
 export default Card;
