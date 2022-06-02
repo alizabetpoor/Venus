a8be32322f732fc34911243e9f23c2e6ef3579f1
alizabetpoor <ali_0_zabetpoor@ymail.com>
2022-06-02 21:20:50 +0430

    refactor

--- src/components/searchInput/index.tsx
+++ src/components/searchInput/index.tsx
@@ -1,45 +1,33 @@
 import React, {useContext, useMemo, useState, useEffect} from 'react';
 import {View, TextInput, TouchableOpacity, Pressable} from 'react-native';
-import FeatherIcon from 'react-native-vector-icons/Feather';
+import {featherIconConstructor} from '../../utils/Constants';
 import {ThemeContext} from '../../themes/ThemeProvider.js';
 import styles from './SearchInput.style.js';
+import {SearchInputProps} from '../../utils/Types';
 import {
   setSearchInput,
   setSearchResults,
 } from '../../store/reducer/searchInputReducer';
 import {connect} from 'react-redux';
 
-type Props = {
-  setSearchValue: (value: string) => void;
-  setSearchIsActive: (value: boolean) => void;
-  setSearchResult: (searchResults: any) => void;
-  users: any;
-};
 const SearchInput: React.FC = ({
   setSearchValue,
   setSearchIsActive,
   setSearchResult,
   users,
-}: Props) => {
+}: SearchInputProps) => {
   const {themeToggle, dark, theme} = useContext(ThemeContext);
 
-  const fatherIconConstructor = (
-    name: string,
-    size: number,
-    color: string,
-  ): React.ReactNode => {
-    return <FeatherIcon name={name} size={size} color={color} />;
-  };
   const [searchText, setSearchText] = useState('');
-  const searchIcon = fatherIconConstructor('search', 20, theme.colors.gray);
-  const sunIcon = fatherIconConstructor('sun', 20, theme.colors.gray);
-  const moonIcon = fatherIconConstructor('moon', 20, theme.colors.gray);
+  const searchIcon = featherIconConstructor('search', 20, theme.colors.gray);
+  const sunIcon = featherIconConstructor('sun', 20, theme.colors.gray);
+  const moonIcon = featherIconConstructor('moon', 20, theme.colors.gray);
   const filterUsers = useMemo(() => {
     if (searchText !== '') {
       const filteredUsers = users.filter(
         (user: any) =>
-          user.name.first.includes(searchText) ||
-          user.name.last.includes(searchText),
+          user.name.first.toLowerCase().includes(searchText.toLowerCase()) ||
+          user.name.last.toLowerCase().includes(searchText.toLowerCase()),
       );
       console.log(filteredUsers, 'filteredusers');
 
